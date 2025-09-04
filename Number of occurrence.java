f172f62a2eb00f51fada3ef0c92bcd3bba85f697
Vaishnavi More <143414333+VaishnaviMore21@users.noreply.github.com>
2025-09-05 04:40:48 +0530

    Create Number of occurrence.java

---
+++ Number of occurrence.java
@@ -0,0 +1,29 @@
+class Solution {
+    int countFreq(int[] arr, int target) {
+        // code here
+         int lb = lowerBound(arr, target);
+        int ub = upperBound(arr, target);
+        return ub - lb;
+    }
+    
+    static int lowerBound(int[] arr, int target) {
+        int lo = 0, hi = arr.length;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] < target) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
+    }
+
+    static int upperBound(int[] arr, int target) {
+        int lo = 0, hi = arr.length;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] <= target) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
+}
+}
+
